a5a970b752a0da721af7febc74b346ad36d641b0
tripledoublev <vincentcharlebois@gmail.com>
2023-11-08 23:40:52 +0100

    Add fade transitions and video preloading logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,56 @@
-<!-- App.svelte -->
 <script>
-  import { onMount } from "svelte";
+  import { onMount, tick } from "svelte";
+  import { tweened } from "svelte/motion";
   import Video from "./Video.svelte";
   import videoCameras from "./video_cameras";
 
-  let currentVideoIndex = 0;
-  let videoUrls = videoCameras.map(video => video.url); // assuming videoCameras is an array of objects with a url field
+  const intervalDuration = 2222; // change this to the initial timer duration
+  const preloadTime = intervalDuration * 0.75;
+  const fadeTime = intervalDuration * 0.99;
 
-  // Function to cycle through videos
-  function cycleVideos() {
-    currentVideoIndex = (currentVideoIndex + 1) % videoUrls.length;
+  let videoUrls = videoCameras.map((video) => video.url);
+  let currentVideoIndex = 0;
+  let nextVideoIndex = 1;
+
+  let currentVideoVisible = true;
+  let nextVideoVisible = false;
+
+  // Function to preload and transition to the next video
+  async function transitionVideos() {
+    // Preload next video at 80% of the interval
+    setTimeout(() => {
+      nextVideoIndex = (currentVideoIndex + 1) % videoUrls.length;
+    }, preloadTime);
+
+    // Fade current video out and next video in at 98% of the interval
+    setTimeout(() => {
+      currentVideoVisible = false;
+      nextVideoVisible = true;
+    }, fadeTime);
+
+    // Switch videos at 100% of the interval
+    setTimeout(() => {
+      currentVideoIndex = nextVideoIndex;
+    }, intervalDuration);
   }
 
-  // Start cycling videos on mount
   onMount(() => {
-    const interval = setInterval(cycleVideos, 3000);
+    const interval = setInterval(transitionVideos, intervalDuration);
+
     return () => {
       clearInterval(interval); // clear interval on component destruction
     };
   });
+
+  // Make sure the videos are loaded in the DOM before playing
+  $: loaded = false;
+  onMount(() => {
+    tick().then(() => {
+      loaded = true;
+    });
+  });
 </script>
 
-<!-- Passing only the current URL to the Video component -->
-<Video url={videoUrls[currentVideoIndex]} description={`Video ${currentVideoIndex + 1}`} />
+
+<Video url={videoUrls[currentVideoIndex]} description={`Video ${currentVideoIndex + 1}`} visible={currentVideoVisible} />
+<Video url={videoUrls[nextVideoIndex]} description={`Video ${nextVideoIndex + 1}`} visible={nextVideoVisible} />
--- src/Video.svelte
+++ src/Video.svelte
@@ -1,10 +1,13 @@
-<!-- Video.svelte -->
 <script>
+  	import { fade } from 'svelte/transition';
+
     export let url;
     export let description;
-    
+    export let fadeTime = 100;
+    export let visible = true;
+
     let videoElement;
-  
+
     $: if (videoElement && url) {
       changeSource();
     }
@@ -19,19 +22,22 @@
       }
     }
   </script>
-  
-  <video
-    bind:this={videoElement}
-    width="100%"
-    muted
-    autoplay
-    preload="auto"
-    aria-label={description}
-    on:stalled={() => console.warn('Video stalled')}
-  >
-    <source src={url} type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
+
+<video
+  bind:this={videoElement}
+  width="100%"
+  muted
+  autoplay
+  preload="auto"
+  aria-label={description}
+  on:stalled={() => console.warn('Video stalled')}
+  in:fade={{ duration: fadeTime }}  
+  out:fade={{ duration: fadeTime }} 
+  class:visible={visible}
+>
+  <source src={url} type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
   
   <style>
     video {
@@ -44,15 +50,10 @@
       width: auto;
       height: auto;
       z-index: -1;
+      transition: opacity 1s ease-in-out;
+
     }
   
-    .loading, .error {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      color: white;
-      z-index: 10;
-    }
+
   </style>
   
